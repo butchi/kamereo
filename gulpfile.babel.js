b2167765c0903ebdad6bb68acdb7861ab1291ebf
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2017-02-11 03:48:28 +0900

    separate module and lib

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,9 @@
 // import
 import gulp from 'gulp';
 import babel from 'gulp-babel';
+import source from 'vinyl-source-stream';
+import browserify from 'browserify';
+import babelify from 'babelify';
 import rename from 'gulp-rename';
 import uglify from 'gulp-uglify';
 import decodecode from 'gulp-decodecode';
@@ -16,24 +19,42 @@ const SRC = './src';
 const HTDOCS = './docs';
 const DEST = './dist';
 
-
+// js-module
 gulp.task('babel', () => {
-  return gulp.src(`${SRC}/js/${APP}.js`)
+  return gulp.src(`${SRC}/js/${APP}-core.js`)
     .pipe(babel())
-    .pipe(gulp.dest(`${DEST}`));
-})
+    .pipe(rename('main.js'))
+    .pipe(gulp.dest('.'))
+  ;
+});
+
+gulp.task('js-module', gulp.series('babel'));
+
+
+// js-lib
+gulp.task('browserify', () => {
+  return browserify(`${SRC}/js/${APP}.js`)
+    .transform(babelify)
+    .bundle()
+    .pipe(source(`${APP}.js`))
+    .pipe(gulp.dest(`${DEST}`))
+  ;
+});
 
 gulp.task('minify', () => {
   return gulp.src(`${DEST}/${APP}.js`)
-    .pipe(uglify({}))
+    .pipe(uglify({
+      preserveComments: 'license',
+    }))
     .pipe(rename(`${APP}.min.js`))
-    .pipe(gulp.dest(`${DEST}`))
+    .pipe(gulp.dest(`${DEST}/js`))
   ;
 });
 
 gulp.task('deco', () => {
   return gulp.src(`${DEST}/${APP}.js`)
     .pipe(decodecode({
+      preserveComments: 'license',
       decoArr: ['赤', '橙', '黄', '緑', '青', '藍', '紫'],
     }))
     .pipe(rename(`${APP}.deco.js`))
@@ -42,7 +63,7 @@ gulp.task('deco', () => {
   ;
 });
 
-gulp.task('js', gulp.series('babel', gulp.parallel('minify', 'deco')));
+gulp.task('js-lib', gulp.series('browserify', gulp.parallel('minify', 'deco')));
 
 
 gulp.task('browser-sync' , () => {
@@ -60,5 +81,5 @@ gulp.task('browser-sync' , () => {
 gulp.task('serve', gulp.series('browser-sync'));
 
 
-gulp.task('build', gulp.series('js'));
+gulp.task('build', gulp.series('js-module', 'js-lib'));
 gulp.task('default', gulp.series('build', 'serve'));
